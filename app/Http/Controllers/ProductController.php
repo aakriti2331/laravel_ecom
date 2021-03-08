bd1b66dac37470f6935eac9ec378e33a4224e8db
aakriti2331 <akriti.aggarwal@adsandurl.com>
2021-03-08 13:40:26 +0530

    3-8-21

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -36,12 +36,8 @@ class ProductController extends Controller
             $result['warranty']=$arr['0']->warranty;
             $result['image']=$arr['0']->image;
             $result['id']=$arr['0']->id;
-            $result['sku']=$arr['0']->sku;
-            $result['mrp']=$arr['0']->mrp;            
-            $result['price']=$arr['0']->price;
-            $result['qty']=$arr['0']->qty;
-            $result['color_id']=$arr['0']->color_id;
-            $result['size_id']=$arr['0']->size_id;
+            $result['productAttrArr']=DB::table('product_attr')->where(['product_id'=>$id])->get();
+
         }else{
             $result['category_id']="";
             $result['name']="";
@@ -55,14 +51,17 @@ class ProductController extends Controller
             $result['technical_spec']="";
             $result['uses']="";
             $result['warranty']="";
-            $result['sku']="";
-            $result['mrp']="";            
-            $result['price']="";
-            $result['qty']="";
-            $result['color_id']="";
-            $result['size_id']="";
             $result['id']=0;
             
+            $result['productAttrArr'][0]['id']='';
+            $result['productAttrArr'][0]['product_id']='';
+            $result['productAttrArr'][0]['sku']='';
+            $result['productAttrArr'][0]['attr_image']='';
+            $result['productAttrArr'][0]['mrp']='';
+            $result['productAttrArr'][0]['price']='';
+            $result['productAttrArr'][0]['qty']='';
+            $result['productAttrArr'][0]['size_id']='';
+            $result['productAttrArr'][0]['color_id']='';
             
         }
         $result['color']=DB::table('colors')->where(['status'=>1])->get();
@@ -115,15 +114,44 @@ class ProductController extends Controller
         $model->uses=$request->post('uses');
         $model->warranty=$request->post('warranty');
         $model->technical_spec=$request->post('technical_spec');
-        $model->keywords=$request->post('sku');
-        $model->keywords=$request->post('price');
-        $model->keywords=$request->post('mrp');
-        $model->keywords=$request->post('image');
-        $model->keywords=$request->post('qty');
+        $model->keywords=$request->post('keywords');
         $model->status=1;
-        
-       //return $model;
         $model->save();
+        $pid=$model->id;
+        /*Product Attr Start*/ 
+        $paidArr=$request->post('paid'); 
+        $skuArr=$request->post('sku'); 
+        $mrpArr=$request->post('mrp'); 
+        $priceArr=$request->post('price'); 
+        $qtyArr=$request->post('qty'); 
+        $size_idArr=$request->post('size_id'); 
+        $color_idArr=$request->post('color_id'); 
+        foreach($skuArr as $key=>$val){
+            $productAttrArr['product_id']=$pid;
+            $productAttrArr['sku']=$skuArr[$key];
+            $productAttrArr['attr_image']='test';
+            $productAttrArr['mrp']=$mrpArr[$key];
+            $productAttrArr['price']=$priceArr[$key];
+            $productAttrArr['qty']=$qtyArr[$key];
+            if($size_idArr[$key]==''){
+                $productAttrArr['size_id']=0;
+            }else{
+                $productAttrArr['size_id']=$size_idArr[$key];
+            }
+
+            if($color_idArr[$key]==''){
+                $productAttrArr['color_id']=0;
+            }else{
+                $productAttrArr['color_id']=$color_idArr[$key];
+            }
+            if($paidArr[$key]!=''){
+                DB::table('product_attr')->where(['id'=>$paidArr[$key]])->update($productAttrArr);
+            }else{
+                DB::table('product_attr')->insert($productAttrArr);
+            }
+            
+        }  
+        /*Product Attr End*/
         $request->session()->flash('message',$msg);
         return redirect('admin/product');
         
@@ -135,7 +163,11 @@ class ProductController extends Controller
         $request->session()->flash('message','Product deleted');
         return redirect('admin/product');
     }
-
+    public function product_attr_delete(Request $request,$paid,$pid){
+        DB::table('product_attr')->where(['id'=>$paid])->delete();
+        return redirect('admin/product/manage_product/'.$pid);
+    }
+    
     public function status(Request $request,$status,$id){
         
         $model=product::find($id);
--- resources/views/admin/manage_product.blade.php
+++ resources/views/admin/manage_product.blade.php
@@ -128,94 +128,128 @@
                             
                               
                         </div>
+                        <h2 class="mb10">Product Attributes</h2>
+            <div class="col-lg-12" id="product_attr_box">
+               @php 
+               $loop_count_num=1;
+               @endphp
+               @foreach($productAttrArr as $key=>$val)
+               @php 
+               $loop_count_prev=$loop_count_num;
+               $pAArr=(array)$val;
+               @endphp
+               <input id="paid" type="hidden" name="paid[]" value="{{$pAArr['id']}}">
+               <div class="card" id="product_attr_{{$loop_count_num++}}">
+                  <div class="card-body">
+                     <div class="form-group">
                         <div class="row">
-                        <div class="col-lg-12">
-                                <div class="card">
-                                    <div class="card-body row">
-                  
-                                    <div class="form-group col-md-2">
-                                                <label for="mrp" class="control-label mb-1">Mrp</label> 
-                                                <input id="mrp" value="" name="mrp" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
+                           <div class="col-md-2">
+                              <label for="sku" class="control-label mb-1"> SKU</label>
+                              <input id="sku" name="sku[]" type="text" class="form-control" aria-required="true" aria-invalid="false" value="{{$pAArr['sku']}}" required>
+                           </div>
+                           <div class="col-md-2">
+                              <label for="mrp" class="control-label mb-1"> MRP</label>
+                              <input id="mrp" name="mrp[]" type="text" class="form-control" aria-required="true" aria-invalid="false" value="{{$pAArr['mrp']}}" required>
+                           </div>
+                           <div class="col-md-2">
+                              <label for="price" class="control-label mb-1"> Price</label>
+                              <input id="price" name="price[]" type="text" class="form-control" aria-required="true" aria-invalid="false" value="{{$pAArr['price']}}" required>
+                           </div>
+                           <div class="col-md-3">
+                              <label for="size_id" class="control-label mb-1"> Size</label>
+                              <select id="size_id" name="size_id[]" class="form-control">
+                                 <option value="">Select</option>
+                                 @foreach($size as $list)
+                                 @if($pAArr['size_id']==$list->id)
+                                    <option value="{{$list->id}}" selected>{{$list->size}}</option>
+                                    @else
+                                    <option value="{{$list->id}}">{{$list->size}}</option>
+                                    @endif
+                                
+                                   
+                                 @endforeach
+                              </select>
+                           </div>
+                           <div class="col-md-3">
+                              <label for="color_id" class="control-label mb-1"> Color</label>
+                              <select id="color_id" name="color_id[]" class="form-control">
+                                 <option value="">Select</option>
+                                 @foreach($color as $list)
+                                    @if($pAArr['size_id']==$list->id)
+                                    <option value="{{$list->id}}" selected>{{$list->color}}</option>
+                                    @else
+                                    <option value="{{$list->id}}">{{$list->color}}</option>
+                                    @endif
+                                 @endforeach
+                              </select>
+                           </div>
+                           <div class="col-md-2">
+                              <label for="qty" class="control-label mb-1"> Qty</label>
+                              <input id="qty" name="qty[]" type="text" class="form-control" aria-required="true" aria-invalid="false" value="{{$pAArr['qty']}}" required>
+                           </div>
+                           <div class="col-md-4">
+                              <label for="attr_image" class="control-label mb-1"> Image</label>
+                              <input id="attr_image" name="attr_image[]" type="file" class="form-control" aria-required="true" aria-invalid="false" required>
+                           </div>
+                           <div class="col-md-2">
+                              <label for="attr_image" class="control-label mb-1"> 
+                              &nbsp;&nbsp;&nbsp;</label>
+                              
+                              @if($loop_count_num==2)
+                                <button type="button" class="btn btn-success btn-lg" onclick="add_more()">
+                                <i class="fa fa-plus"></i>&nbsp; Add</button>
+                              @else
+                              <a href="{{url('admin/product/product_attr_delete/')}}/{{$pAArr['id']}}/{{$id}}"><button type="button" class="btn btn-danger btn-lg">
+                                <i class="fa fa-minus"></i>&nbsp; Remove</button></a>
+                              @endif  
 
-                                            </div>
-                                            <div class="form-group col-md-2">
-                                                <label for="sku" class="control-label mb-1">Sku</label>
-                                                <input id="sku" value="" name="sku" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
-                                            </div>
-                                            <div class="form-group col-md-2">
-                                                <label for="price" class="control-label mb-1"> Price(Rs)</label>
-                                                <input id="price" value="" name="price" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
-                                            
-                                            </div>
-                                            <div class="form-group col-md-2">
-                                                <label for="image" class="control-label mb-1"> image</label>
-                                                <input id="image" value="" name="image" type="file" class="form-control" aria-required="true" aria-invalid="false" required>
-                                            
-                                            </div>
-                                            <div class="form-group col-md-2">
-                                                <label for="color" class="control-label mb-1">Color</label>
-                                                <select id="color" value="{{$color_id}}" name="color"  class="form-control">
-                                              
-                                                @foreach($color as $list)
-                                                @if($color_id==$list->id)
-                                                <option selected value="{{$list->id}}">
-                                                {{$list->color}}
-                                                @else
-                                                <option value="{{$list->id}}">
-                                                {{$list->color}}</option>
-                                                @endif
-                                                @endforeach
-                                                </select>
-                                           
-                                                @error('name')
-                                                <div class="alert alert-danger" role="alert">
-                                                    {{$message}}		
-                                                </div>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group col-md-2">
-                                                <label for="size" class="control-label mb-1">Sizes</label>
-                                                <select id="size" value="{{$size_id}}" name="size"  class="form-control">
-                                              
-                                                @foreach($size as $list)
-                                                @if($size_id==$list->id)
-                                                <option selected value="{{$list->id}}">
-                                                {{$list->size}}
-                                                @else
-                                                <option value="{{$list->id}}">
-                                                {{$list->size}}</option>
-                                                @endif
-                                                @endforeach
-                                                </select>
-                                           
-                                                @error('name')
-                                                <div class="alert alert-danger" role="alert">
-                                                    {{$message}}		
-                                                </div>
-                                                @enderror
-                                            </div>
-                                            <div class="form-group col-md-2">
-                                                <label for="qty" class="control-label mb-1">Stock</label>
-                                                <input id="qty" value="" name="qty" type="text" class="form-control" aria-required="true" aria-invalid="false" required>
-                                            
-                                            </div>
-                                    </div>
-                                    </div>
-                                    </div>                         
+                           </div>
                         </div>
-                        <div>
+                     </div>
+                  </div>
+               </div>
+               @endforeach
+            </div>
+         </div>
+         <div>
+            <button id="payment-button" type="submit" class="btn btn-lg btn-info btn-block">
+            Submit
+            </button>
+         </div>
+         <input type="hidden" name="id" value="{{$id}}"/>
+      </form>
+   </div>
+</div>
+<script>
+   var loop_count=1; 
+   function add_more(){
+       loop_count++;
+       var html='<input id="paid" type="text" name="paid[]" ><div class="card" id="product_attr_'+loop_count+'"><div class="card-body"><div class="form-group"><div class="row">';
 
+       html+='<div class="col-md-2"><label for="sku" class="control-label mb-1"> SKU</label><input id="sku" name="sku[]" type="text" class="form-control" aria-required="true" aria-invalid="false" required></div>'; 
 
-                                                <button id="payment-button" type="submit" class="btn btn-lg btn-info btn-block">
-                                                    Submit
-                                                </button>
-                                            </div>
-                                            <input type="hidden" name="id" value="{{$id}}"/>
-                        </form>
-        </div>
+       html+='<div class="col-md-2"><label for="mrp" class="control-label mb-1"> MRP</label><input id="mrp" name="mrp[]" type="text" class="form-control" aria-required="true" aria-invalid="false" required></div>'; 
 
-       
+       html+='<div class="col-md-2"><label for="price" class="control-label mb-1"> Price</label><input id="price" name="price[]" type="text" class="form-control" aria-required="true" aria-invalid="false" required></div>';
 
-    </div>
-                        
+       var size_id_html=jQuery('#size_id').html(); 
+       html+='<div class="col-md-3"><label for="size_id" class="control-label mb-1"> Size</label><select id="size_id" name="size_id[]" class="form-control">'+size_id_html+'</select></div>';
+
+       var color_id_html=jQuery('#color_id').html(); 
+       html+='<div class="col-md-3"><label for="color_id" class="control-label mb-1"> Color</label><select id="color_id" name="color_id[]" class="form-control" >'+color_id_html+'</select></div>';
+
+       html+='<div class="col-md-2"><label for="qty" class="control-label mb-1"> Qty</label><input id="qty" name="qty[]" type="text" class="form-control" aria-required="true" aria-invalid="false" required></div>';
+
+       html+='<div class="col-md-4"><label for="attr_image" class="control-label mb-1"> Image</label><input id="attr_image" name="attr_image[]" type="file" class="form-control" aria-required="true" aria-invalid="false" required></div>';
+
+       html+='<div class="col-md-2"><label for="attr_image" class="control-label mb-1"> &nbsp;&nbsp;&nbsp;</label><button type="button" class="btn btn-danger btn-lg" onclick=remove_more("'+loop_count+'")><i class="fa fa-minus"></i>&nbsp; Remove</button></div>'; 
+
+       html+='</div></div></div></div>';
+
+       jQuery('#product_attr_box').append(html)
+   }
+   function remove_more(loop_count){
+        jQuery('#product_attr_'+loop_count).remove();
+   }
+</script>
 @endsection
